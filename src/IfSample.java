c45d95686d0e2f1384301b7cdfdfd790da74d818
Hovhannes Gasparyan <hovhannes2910@gmail.com>
2025-09-07 22:14:38 +0400

    Initial commit

---
+++ src/IfSample.java
@@ -0,0 +1,16 @@
+class IfSample {
+    public static void main(String[] args) {
+        int x, y;
+        x = 10;
+        y = 20;
+        if (x < y) System.out.println("x меньше у");
+
+        x = x * 2;
+        if (x == y) System.out.println("x теперь равно у");
+
+        x = x * 2;
+        if (x > y) System.out.println("x теперь больше y");
+
+        if (x == y) System.out.println("вы не увидите этого");
+    }
+}
