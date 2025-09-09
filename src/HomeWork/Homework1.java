3cf9a9b0fc9d09fb47c70d9b70a2d1f98d8895d9
Hovhannes Gasparyan <hovhannes2910@gmail.com>
2025-09-09 21:45:21 +0400

    Home work 1, Example

---
+++ src/HomeWork/Homework1.java
@@ -0,0 +1,36 @@
+package HomeWork;
+
+public class Homework1 {
+    public static void main(String[] args) {
+        int x, y;
+
+        x = 3;
+        y = 5;
+
+        if (x < y) {
+            System.out.println(" Y болmше X");
+        }
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+
+        int a, b, c;
+
+        a = 5;
+        b = 7;
+        c = a + b;
+
+
+        System.out.println("a + b равно " + c);
+
+        int n = 3;
+        for (int w = 1; w <= 10; w++) {
+
+            System.out.println(n + " * " + w + " = " + (n * w));
+
+        }
+
+
+    }
+}
